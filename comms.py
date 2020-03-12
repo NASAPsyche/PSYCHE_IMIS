f31774c8ff01c59394a39323854c9e8266a5745b
Andrew Murza <androiski@gmail.com>
2020-03-11 17:34:36 -0700

    Update comms.py

--- comms.py
+++ comms.py
@@ -21,4 +21,24 @@ channel_0 = pca.channels[0]
 channel_0.duty_cycle = 0xffff
 
 
+#SETUP comms on IMU - 9DOF FXOS8700 + FXAS21002
+#example https://learn.adafruit.com/nxp-precision-9dof-breakout/python-circuitpython
+import board
+import busio
+import adafruit_fxos8700
+import adafruit_fxas21002c
 
+
+i2c = busio.I2C(board.SCL, board.SDA)
+fxos = adafruit_fxos8700.FXOS8700(i2c)
+fxas = adafruit_fxas21002c.FXAS21002C(i2c)
+
+# Read
+accel_x, accel_y, accel_z = sensor.accelerometer
+mag_x, mag_y, mag_z = sensor.magnetometer
+gyro_x, gyro_y, gyro_z = sensor.gyroscope
+    
+# Print values.
+print('Acceleration (m/s^2): ({0:0.3f}, {1:0.3f}, {2:0.3f})'.format(accel_x, accel_y, accel_z)) # you could also do .format(*fxos.accelerometer)
+print('Magnetometer (uTesla): ({0:0.3f}, {1:0.3f}, {2:0.3f})'.format(mag_x, mag_y, mag_z))
+print('Gyroscope (radians/s): ({0:0.3f},  {1:0.3f},  {2:0.3f})'.format(gyro_x, gyro_y, gyro_z))
